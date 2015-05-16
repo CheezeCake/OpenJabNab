2a87df5d8fe66ea479341741a75e904704f7f9ed
CheezeCake <emmanuel.nicolet@gmail.com>
2015-05-16 21:51:43 +0200

    bug fixes

--- server/plugins/agenda/plugin_agenda.cpp
+++ server/plugins/agenda/plugin_agenda.cpp
@@ -13,7 +13,7 @@ Q_EXPORT_PLUGIN2(plugin_agenda, PluginAgenda)
 
 PluginAgenda::PluginAgenda():PluginInterface("agenda", "agenda plugin")
 {
-	if (sqlite3_open("file:agenda.db", &dbHandle) != SQLITE_OK)
+	if (sqlite3_open("agenda.db", &dbHandle) != SQLITE_OK)
 	{
 		LogError(QString("Error opening agenda db file").arg(sqlite3_errmsg(dbHandle)));
 		dbHandle = 0;
@@ -21,9 +21,15 @@ PluginAgenda::PluginAgenda():PluginInterface("agenda", "agenda plugin")
 	else
 	{
 		LogInfo("agenda.db successfully opened");
+		char* err = 0;
 		sqlite3_exec(dbHandle,
-				"create table if not exists events (id integer primary key autoincrement, date text, event text)",
-				0, 0, 0);
+				"create table if not exists events (id int primary key, date text, event text);",
+				0, 0, &err);
+		if (err)
+		{
+			LogError(QString("Error creating table events : %1").arg(err));
+			sqlite3_free(err);
+		}
 	}
 }
 
@@ -56,31 +62,33 @@ bool PluginAgenda::OnClick(Bunny* b, PluginInterface::ClickType)
 
 		QString str = PocketSphinx::getLastRecognized();
 		str.remove(QRegExp("^agenda "));
-		LogInfo(QString("agenda str = %1").arg(str));
+		LogInfo(QString("agenda str = '%1'").arg(str));
 
 		int code;
 		sqlite3_stmt *stmt;
 
-		QRegExp add("ajouter (.+) le (.+)( a ([0-9]+) heure ([0-9]+)?)?");
-		QRegExp list("[0-9]+ .+");
+		QRegExp add("ajouter (.+) le ([^ ]+ [^ ]+)( a (.+) heure (.+)?)?");
+		QRegExp list(".+ [a-z]+");
 		QRegExp remove("supprimer ([0-9]+)");
 
 		if (add.exactMatch(str))
 		{
-			std::string sql("inset into events (date, event) values(?, ?)");
-			code = sqlite3_prepare_v2(dbHandle, sql.c_str(), sql.size(), &stmt, 0);
+			const char sql[] = "insert into events (date, event) values(?, ?)";
+			code = sqlite3_prepare_v2(dbHandle, sql, -1, &stmt, 0);
 			if (code != SQLITE_OK) {
 				LogError(QString(sqlite3_errstr(code)));
 				return true;
 			}
 
 			QString cap = add.cap(3) + " " + add.cap(1); // [ a ..h..] event
-			sqlite3_bind_text(stmt, 2, cap.toStdString().c_str(), cap.length(), 0);
+			sqlite3_bind_text(stmt, 2, cap.toStdString().c_str(), -1, SQLITE_TRANSIENT);
 
-			cap = add.cap(2);
-			sqlite3_bind_text(stmt, 1, cap.toStdString().c_str(), cap.length(), 0);
+			QString cap1 = add.cap(2);
+			sqlite3_bind_text(stmt, 1, cap1.toStdString().c_str(), -1, SQLITE_TRANSIENT);
 
-			file = TTSManager::CreateNewSound((sqlite3_step(stmt) == SQLITE_OK)
+			LogInfo(QString("%1, %2").arg(cap, cap1));
+
+			file = TTSManager::CreateNewSound((sqlite3_step(stmt) != SQLITE_ERROR)
 					? QString("evenement cree avec l'identifiant %1").arg(sqlite3_last_insert_rowid(dbHandle))
 					: "Erreur lors de l'insertion de l'evenement dans l'agenda",
 					"julie");
@@ -89,20 +97,21 @@ bool PluginAgenda::OnClick(Bunny* b, PluginInterface::ClickType)
 		}
 		else if (list.exactMatch(str))
 		{
-			std::string sql("select event from events where date = ?");
-			code = sqlite3_prepare_v2(dbHandle, sql.c_str(), sql.size(), &stmt, 0);
+			const char sql[] = "select event from events where date = ?";
+			code = sqlite3_prepare_v2(dbHandle, sql, -1, &stmt, 0);
 			if (code != SQLITE_OK) {
 				LogError(QString(sqlite3_errstr(code)));
 				return true;
 			}
 
-			sqlite3_bind_text(stmt, 1, str.toStdString().c_str(), str.length(), 0);
+			LogInfo(QString("str.std.c_str = '%1'").arg(str.toStdString().c_str()));
+
+			sqlite3_bind_text(stmt, 1, str.toStdString().c_str(), -1, SQLITE_TRANSIENT);
 
 			QString eventList;
 			while (sqlite3_step(stmt) == SQLITE_ROW)
 			{
-				sqlite3_value* value = sqlite3_column_value(stmt, 2);
-				QString event(reinterpret_cast<const char*>(sqlite3_value_text(value)));
+				QString event(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0)));
 				if (!event.isEmpty())
 				{
 					std::cout << event.toStdString() << '\n';
@@ -116,15 +125,15 @@ bool PluginAgenda::OnClick(Bunny* b, PluginInterface::ClickType)
 		}
 		else if (remove.exactMatch(str))
 		{
-			std::string sql("delete from events where id = ?");
-			code = sqlite3_prepare_v2(dbHandle, sql.c_str(), sql.size(), &stmt, 0);
+			const char sql[] = "delete from events where id = ?";
+			code = sqlite3_prepare_v2(dbHandle, sql, -1, &stmt, 0);
 			if (code != SQLITE_OK) {
 				LogError(QString(sqlite3_errstr(code)));
 				return true;
 			}
 
 			QString cap = remove.cap(1);
-			sqlite3_bind_text(stmt, 1, cap.toStdString().c_str(), cap.length(), 0);
+			sqlite3_bind_text(stmt, 1, cap.toStdString().c_str(), cap.length(), SQLITE_TRANSIENT);
 
 			file = TTSManager::CreateNewSound((sqlite3_step(stmt) == SQLITE_OK)
 					? QString("evenement %1 supprime").arg(cap)
