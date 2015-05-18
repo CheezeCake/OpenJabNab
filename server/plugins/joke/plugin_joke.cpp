b48c4f7626bc2d19b5884d759f1a38df534098ea
JulienAJ <gammer84@hotmail.fr>
2015-05-18 19:54:40 +0200

    add jokes plugin, some jokes in resources, TODO: change directory (maybe in http dirs)

---
+++ server/plugins/joke/plugin_joke.cpp
@@ -0,0 +1,52 @@
+#include "plugin_joke.h"
+#include "settings.h"
+#include "bunny.h"
+#include "context.h"
+#include "ttsmanager.h"
+#include "messagepacket.h"
+
+Q_EXPORT_PLUGIN2(plugin_joke, PluginJoke)
+
+PluginJoke::PluginJoke():PluginInterface("joke", "joke plugin")
+{
+}
+
+PluginJoke::~PluginJoke() {}
+
+bool PluginJoke::OnClick(Bunny* b, PluginInterface::ClickType)
+{
+	if(b->IsIdle() && Context::getAvailability())
+	{
+		// TODO Check the directory
+		QString jokeDirPath = "/resources/blagues";
+		QDir jokeDir(jokeDirPath);
+		if(jokeDir)
+		{
+			QStringList list = jokeDir->entryList();
+			if(list.count())
+			{
+				int rand = qrand % list.count();
+				QFile file(list.at(rand));
+				if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+				{
+					QByteArray text("");
+					while (!file.atEnd())
+					{
+						QByteArray text += file.readLine();
+					}
+					QString textMessage(text);
+					QString voice = b->GetPluginSetting(GetName(), "voice", "tts").toString();
+					QByteArray voiceFile = TTSManager::CreateNewSound(textMessage, "julie");
+
+					if(!voiceFile.isNull())
+					{
+						QByteArray message = "MU "+file+"\nPL 3\nMW\n";
+						b->SendPacket(MessagePacket(message));
+						return true;
+					}
+				}
+			}
+		}
+	}
+	return false;
+}
---
+++ server/plugins/joke/plugin_joke.h
@@ -0,0 +1,18 @@
+#ifndef _PLUGINJOKE_H_
+#define _PLUGINJOKE_H_
+
+#include "plugininterface.h"
+	
+class PluginJoke : public PluginInterface
+{
+	Q_OBJECT
+	Q_INTERFACES(PluginInterface)
+
+public:
+	PluginJoke();
+	virtual ~PluginJoke();
+
+	bool OnClick(Bunny* b, PluginInterface::ClickType)
+};
+
+#endif
